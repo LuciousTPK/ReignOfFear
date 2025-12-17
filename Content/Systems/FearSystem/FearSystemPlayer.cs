b387762160b7288aae649d1cca59e84af72cdebd
Luci <lucioustpk@gmail.com>
2025-12-17 03:08:42 -0600

    Initial commit

---
+++ Content/Systems/FearSystem/FearSystemPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Terraria.ModLoader;
+
+namespace ReignOfFear.Content.Systems.FearSystem
+{
+    enum PhobiaID
+    {
+        Kinemortophobia,
+        Phasmophobia,
+        Skelephobia
+    }
+
+    internal class FearSystemPlayer : ModPlayer
+    {
+        Dictionary<PhobiaID, PlayerPhobiaState> playerPhobiaData = new Dictionary<PhobiaID, PlayerPhobiaState>();
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            foreach (PhobiaID phobia in Enum.GetValues<PhobiaID>())
+            {
+                playerPhobiaData[phobia] = new PlayerPhobiaState();
+                //We'd probably have some lines down here that took info from a Save/Load file and
+                //Applied the information to the PlayerPhobiaState instance, but since we don't have
+                //That we will have to play pretend and just say that logic is here
+            }
+        }
+    }
+}
---
+++ Content/Systems/FearSystem/PlayerPhobiaState.cs
@@ -0,0 +1,12 @@
+﻿namespace ReignOfFear.Content.Systems.FearSystem
+{
+    internal class PlayerPhobiaState
+    {
+        public float fearPoints;
+        public bool hasPhobia;
+        public bool isImmune;
+        public bool isBurden;
+        public int currentRank;
+        public List<PhobiaDebuff> activeDebuffs = new List<PhobiaDebuff>();
+    }
+}
